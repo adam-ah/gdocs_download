105252922dacda39ea7569187e9fe802a6e79501
Adam Horvath <adam@nethosting.hu>
2020-06-07 09:17:07 +1000

    Updating to new Google API.

--- gdocs_download2.py
+++ gdocs_download2.py
@@ -2,41 +2,46 @@
 """
 Recursively extracts the text from a Google Doc.
 """
-from __future__ import print_function
-
 import argparse
 import os
+import os.path
+import pickle
 import sys
 
-from apiclient import discovery
-from httplib2 import Http
-from oauth2client import client
-from oauth2client import file as oauth2clientfile
-from oauth2client import tools
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 SCOPES = 'https://www.googleapis.com/auth/documents.readonly'
 DISCOVERY_DOC = 'https://docs.googleapis.com/$discovery/rest?version=v1'
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 CREDENTIALS_FILENAME = os.path.join(BASE_DIR, 'credentials.json')
-TOKEN_FILENAME = os.path.join(BASE_DIR, 'token.json')
+TOKEN_FILENAME = os.path.join(BASE_DIR, 'token.pickle')
 
 def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth 2.0 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
+    """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
     """
-    store = oauth2clientfile.Storage(TOKEN_FILENAME)
-    credentials = store.get()
-
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CREDENTIALS_FILENAME, SCOPES)
-        credentials = tools.run_flow(flow, store)
-    return credentials
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(TOKEN_FILENAME):
+        with open(TOKEN_FILENAME, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_FILENAME, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(TOKEN_FILENAME, 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
 
 def read_paragraph_element(element):
     """Returns the text in the given ParagraphElement.
@@ -50,7 +55,7 @@ def read_paragraph_element(element):
     return text_run.get('content')
 
 
-def read_strucutural_elements(elements):
+def read_structural_elements(elements):
     """Recurses through a list of Structural Elements to read a document's text where text may be
         in nested elements.
 
@@ -61,8 +66,8 @@ def read_strucutural_elements(elements):
     for value in elements:
         if 'paragraph' in value:
             elements = value.get('paragraph').get('elements')
-            for elem in elements:
-                text += read_paragraph_element(elem)
+            for element in elements:
+                text += read_paragraph_element(element)
         elif 'table' in value:
             # The text in table cells are in nested Structural Elements and tables may be
             # nested.
@@ -70,24 +75,23 @@ def read_strucutural_elements(elements):
             for row in table.get('tableRows'):
                 cells = row.get('tableCells')
                 for cell in cells:
-                    text += read_strucutural_elements(cell.get('content'))
+                    text += read_structural_elements(cell.get('content'))
         elif 'tableOfContents' in value:
             # The text in the TOC is also in a Structural Element.
             toc = value.get('tableOfContents')
-            text += read_strucutural_elements(toc.get('content'))
+            text += read_structural_elements(toc.get('content'))
     return text
 
 
 def get_document(document_id):
     """Uses the Docs API to print out the text of a document."""
     credentials = get_credentials()
-    http = credentials.authorize(Http())
-    docs_service = discovery.build(
-        'docs', 'v1', http=http, discoveryServiceUrl=DISCOVERY_DOC)
+    # http = credentials.authorize(Http())
+    docs_service = build('docs', 'v1', credentials=credentials)
     doc = docs_service.documents().get(documentId=document_id).execute()
     doc_content = doc.get('body').get('content')
     # print(doc_content)
-    return read_strucutural_elements(doc_content)
+    return read_structural_elements(doc_content)
 
 def main():
     parser = argparse.ArgumentParser(
